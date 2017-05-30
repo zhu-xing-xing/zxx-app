99ed12eb63af8bf67576fb60687a0c2ad85a2618
zhu-xing-xing <zhuxingxing3721@163.com>
2017-05-30 11:51:26 +0800

    4.Slider

---
+++ app/components/slider/index.js
@@ -0,0 +1,146 @@
+import React,{Component} from 'react';
+import ReactSwipe from 'react-swipe';  //轮播图插件
+import './index.less';
+
+export default class Slider extends Component{
+    render(){
+        return (
+        	<div>
+		        <ReactSwipe className="carousel" swipeOptions={{continuous: true}}>
+			        <div>
+				        <ul>
+					        <li>
+						        <i className="iconfont icon-coupons"></i>
+						        <span>美食</span>
+					        </li>
+					        <li>
+						        <i className="iconfont icon-coupons"></i>
+						        <span>美食</span>
+					        </li>
+					        <li>
+						        <i className="iconfont icon-coupons"></i>
+						        <span>美食</span>
+					        </li>
+					        <li>
+						        <i className="iconfont icon-coupons"></i>
+						        <span>美食</span>
+					        </li>
+					        <li>
+						        <i className="iconfont icon-coupons"></i>
+						        <span>美食</span>
+					        </li>
+					        <li>
+						        <i className="iconfont icon-coupons"></i>
+						        <span>美食</span>
+					        </li>
+					        <li>
+						        <i className="iconfont icon-coupons"></i>
+						        <span>美食</span>
+					        </li>
+					        <li>
+						        <i className="iconfont icon-coupons"></i>
+						        <span>美食</span>
+					        </li>
+					        <li>
+						        <i className="iconfont icon-coupons"></i>
+						        <span>美食</span>
+					        </li>
+					        <li>
+						        <i className="iconfont icon-coupons"></i>
+						        <span>美食</span>
+					        </li>
+				        </ul>
+			        </div>
+			        <div>
+				        <ul>
+					        <li>
+						        <i className="iconfont icon-coupons"></i>
+						        <span>美食</span>
+					        </li>
+					        <li>
+						        <i className="iconfont icon-coupons"></i>
+						        <span>美食</span>
+					        </li>
+					        <li>
+						        <i className="iconfont icon-coupons"></i>
+						        <span>美食</span>
+					        </li>
+					        <li>
+						        <i className="iconfont icon-coupons"></i>
+						        <span>美食</span>
+					        </li>
+					        <li>
+						        <i className="iconfont icon-coupons"></i>
+						        <span>美食</span>
+					        </li>
+					        <li>
+						        <i className="iconfont icon-coupons"></i>
+						        <span>美食</span>
+					        </li>
+					        <li>
+						        <i className="iconfont icon-coupons"></i>
+						        <span>美食</span>
+					        </li>
+					        <li>
+						        <i className="iconfont icon-coupons"></i>
+						        <span>美食</span>
+					        </li>
+					        <li>
+						        <i className="iconfont icon-coupons"></i>
+						        <span>美食</span>
+					        </li>
+					        <li>
+						        <i className="iconfont icon-coupons"></i>
+						        <span>美食</span>
+					        </li>
+				        </ul>
+			        </div>
+			        <div>
+				        <ul>
+					        <li>
+						        <i className="iconfont icon-coupons"></i>
+						        <span>美食</span>
+					        </li>
+					        <li>
+						        <i className="iconfont icon-coupons"></i>
+						        <span>美食</span>
+					        </li>
+					        <li>
+						        <i className="iconfont icon-coupons"></i>
+						        <span>美食</span>
+					        </li>
+					        <li>
+						        <i className="iconfont icon-coupons"></i>
+						        <span>美食</span>
+					        </li>
+					        <li>
+						        <i className="iconfont icon-coupons"></i>
+						        <span>美食</span>
+					        </li>
+					        <li>
+						        <i className="iconfont icon-coupons"></i>
+						        <span>美食</span>
+					        </li>
+					        <li>
+						        <i className="iconfont icon-coupons"></i>
+						        <span>美食</span>
+					        </li>
+					        <li>
+						        <i className="iconfont icon-coupons"></i>
+						        <span>美食</span>
+					        </li>
+					        <li>
+						        <i className="iconfont icon-coupons"></i>
+						        <span>美食</span>
+					        </li>
+					        <li>
+						        <i className="iconfont icon-coupons"></i>
+						        <span>美食</span>
+					        </li>
+				        </ul>
+			        </div>
+		        </ReactSwipe>
+	        </div>
+        )
+    }
+}
