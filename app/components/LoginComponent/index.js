296c3164daea4a216f60877596c08ba8fa1cf308
zhu-xing-xing <zhuxingxing3721@163.com>
2017-05-31 17:18:03 +0800

    19.Login登录页

--- app/components/LoginComponent/index.js
+++ app/components/LoginComponent/index.js
@@ -1,10 +1,34 @@
-import React,{Component} from 'react';
-export default class LoginComponent extends Component{
-    render(){
-        return (
-            <div>
-	            LoginComponent
-            </div>
-        )
-    }
+import React, {Component} from 'react';
+export default class LoginComponent extends Component {
+	constructor() {
+		super();
+		this.state = {
+			val: ''
+		}
+	}
+	
+	render() {
+		return (
+			<div>
+				{/*获取输入框的值 ref 非受控 ;
+				 受控onchange+state   事件源*/}
+				<input type="text" value={this.state.val}
+				       onChange={this.changeValue.bind(this)}
+				       placeholder="请输入用户名"
+				/>
+				<button onClick={this.login.bind(this)}>登录</button>
+			</div>
+		)
+	}
+	
+	changeValue(e) {
+		this.setState({
+			val: e.target.value
+		});
+	}
+	
+	login() {
+		alert(this.state.val);
+	}
+	
 }
