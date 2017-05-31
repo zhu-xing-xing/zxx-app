98c170866f45d3f0d6c2efe92d460792d51f3ea0
zhu-xing-xing <zhuxingxing3721@163.com>
2017-05-31 09:58:19 +0800

    12,点击获取更多

---
+++ app/components/LoadMore/index.js
@@ -0,0 +1,24 @@
+import React,{Component} from 'react';
+import './index.less';
+
+export default class LoadMore extends Component{
+    render(){
+        return (
+            <div className="has-more">
+	            {
+	            	this.props.hasMore?
+		            <div onClick={this.loadMore.bind(this)}>加载更多</div>
+		            :
+		            <div>我是有底线的</div>
+	            }
+            </div>
+        )
+    }
+    
+    loadMore(){  //调用父级传递过来的loadMore函数
+	    if(this.props.isLoading){
+	    	return;
+	    }
+    	this.props.loadMore();
+    }
+}
--- app/containers/Home/subpage/List.js
+++ app/containers/Home/subpage/List.js
@@ -3,13 +3,16 @@ import React, {Component} from 'react';
 import {getList} from '../../../fetch/home';
 import ListComponent from "../../../components/ListComponent/index";
 import './index.less';
+import LoadMore from "../../../components/LoadMore/index";
 
 export default class List extends Component {
 	constructor() {
 		super();
 		this.state = {
-			hasMore: true,
-			data: []
+			hasMore: true, //是否有更多
+			data: [],  //所有数据
+			page:0,   //页码
+			isLoading:true  //是否正在加载
 		}
 	}
 	
@@ -19,10 +22,10 @@ export default class List extends Component {
 				<h3>附近美食</h3>
 				{/*将数据传递给ListComponent组件,让其显示*/}
 				{this.state.data.length ?
-					<ListComponent data={this.state.data}/>
+					<ListComponent data={this.state.data} />
 					:
-					<div>正在加载</div>
-				}
+					<div>正在加载</div>}
+					<LoadMore hasMore={this.state.hasMore} loadMore={this.loadMore.bind(this)} isLoading={this.state.isLoading}/>
 			</div>
 		)
 	}
@@ -31,13 +34,24 @@ export default class List extends Component {
 		this.processData(getList(this.props.cityName, 0));//通过fetch/home/index.js中的getList,获取列表数据
 	}
 	
+	//需要在当前写一个加载更多的函数,传递给loadmore,当点击按钮,触发我们传递函数
+	loadMore(){
+		this.setState({
+			page:this.state.page+1,
+			isLoading:true  //每次加在更多时,动作就应该为正在加载
+		},()=>{   //这个函数可以获取到最新的页码状态
+			this.processData(getList(this.props.cityName, this.state.page));
+		})
+	}
+	
 	//处理成功后的逻辑
 	processData(result) {  //result(是promise对象,才可以用.then方法)=获取到的列表数据 result是一个对象
 		//先把promise对象,转化为json对象 ,再解构赋值获得hasMore, data
 		result.then(res => res.json()).then(({hasMore, data}) => {
 			this.setState({
 				hasMore,
-				data
+				data:this.state.data.concat(data),
+				isLoading:false
 			})
 		})
 	}
