c451d549807fdb191a6ec2d8420dc06980ad871e
zhu-xing-xing <zhuxingxing3721@163.com>
2017-05-31 13:02:34 +0800

    16,用户详情页面结构

--- app/components/InfoComponent/index.js
+++ app/components/InfoComponent/index.js
@@ -6,13 +6,13 @@ export default class InfoComponent extends Component{
     render(){
 	    let {img,star,desc,title,subTitle,price} = this.props.data;
 	    return (
-	    	<div>
+	    	<div className="info-component">
 			    
-			    <div>
+			    <div className="info-list">
 				    <img src={img} alt=""/>
 				    <div>
 					    <h4>{title}</h4>
-					    <div>
+					    <div className="info-star">
 						    {/*Star组件要接受一个点亮的星星数*/}
 						    <Star count={star}/>
 						    <span>￥{price}</span>
@@ -20,7 +20,7 @@ export default class InfoComponent extends Component{
 					    <p>{subTitle}</p>
 				    </div>
 			    </div>
-			    
+			    <hr />
 			    {/*将内容转化为html,插入到页面中*/}
 			    <div dangerouslySetInnerHTML={{__html:desc}}></div>
 		    </div>
